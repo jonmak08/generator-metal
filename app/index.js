07ba39dac15ee7308c4e8f49c3cb69efbfff2dd1
Zeno Rocha <hi@zenorocha.com>
2015-04-03 09:50:46 -0700

    Rephrases messages and source formatting

--- app/index.js
+++ app/index.js
@@ -7,21 +7,17 @@ var yosay  = require('yosay');
 
 module.exports = yeoman.generators.Base.extend({
   initializing: function () {
-    this.pkg = require('../package.json');
-  },
-
-  prompting: function () {
     var done = this.async();
 
-    this.log(yosay('Welcome to ' + chalk.green('Metal') + ' Generator!'));
+    this.log(yosay('Welcome, let\'s generate a ' + chalk.green('Metal') + ' component!'));
 
     var prompts = [{
       type: 'input',
       name: 'componentName',
-      message: 'What\'s your component class name?',
+      message: 'How do you want to name this component?',
       validate: function(input) {
         if (!input) {
-          return 'You need to provide a component class name.';
+          return 'You need to provide a component name.';
         }
 
         return true;
@@ -36,29 +32,25 @@ module.exports = yeoman.generators.Base.extend({
     }.bind(this));
   },
 
-  writing: {
-    source: function () {
-      this.fs.copy(this.templatePath('src/jshintrc'),
-        this.destinationPath('src/.jshintrc'));
+  writing: function() {
+    this.fs.copy(this.templatePath('src/jshintrc'),
+      this.destinationPath('src/.jshintrc'));
 
-      this.fs.copyTpl(this.templatePath('src/_Boilerplate.js'),
-        path.join(this.destinationRoot(), 'src', this.capitalizeName + '.js'),
-        { capitalizeName: this.capitalizeName,
-          lowercaseName: this.lowercaseName
-        });
+    this.fs.copyTpl(this.templatePath('src/_Boilerplate.js'),
+      path.join(this.destinationRoot(), 'src', this.capitalizeName + '.js'),
+      { capitalizeName: this.capitalizeName,
+        lowercaseName: this.lowercaseName
+      });
 
-      this.fs.copyTpl(this.templatePath('src/_Boilerplate.soy'),
-        path.join(this.destinationRoot(), 'src', this.capitalizeName + '.soy'),
-        { capitalizeName: this.capitalizeName });
-    },
+    this.fs.copyTpl(this.templatePath('src/_Boilerplate.soy'),
+      path.join(this.destinationRoot(), 'src', this.capitalizeName + '.soy'),
+      { capitalizeName: this.capitalizeName });
 
-    test: function () {
-      this.fs.copy(this.templatePath('test/jshintrc'),
-        this.destinationPath('test/.jshintrc'));
+    this.fs.copy(this.templatePath('test/jshintrc'),
+      this.destinationPath('test/.jshintrc'));
 
-      this.fs.copyTpl(this.templatePath('test/_Boilerplate.js'),
-        path.join(this.destinationRoot(), 'test', this.capitalizeName + '.js'),
-        { capitalizeName: this.capitalizeName });
-    }
+    this.fs.copyTpl(this.templatePath('test/_Boilerplate.js'),
+      path.join(this.destinationRoot(), 'test', this.capitalizeName + '.js'),
+      { capitalizeName: this.capitalizeName });
   }
 });
--- test/test-app.js
+++ test/test-app.js
@@ -3,13 +3,10 @@
 var path = require('path');
 var assert = require('yeoman-generator').assert;
 var helpers = require('yeoman-generator').test;
-var os = require('os');
 
 describe('metal:app', function () {
   before(function (done) {
     helpers.run(path.join(__dirname, '../app'))
-      .inDir(path.join(os.tmpdir(), './temp-test'))
-      .withOptions({ 'skip-install': true })
       .withPrompt({
         componentName: 'boilerplate'
       })
